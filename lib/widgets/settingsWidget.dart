9e12f1cc9508a82b62f7b048b0a3ea70ea3f256d
Abhijit Apotikar <abhijit.apotikar156@gmail.com>
2020-10-25 11:36:04 +0530

    more options added to settings

--- lib/widgets/settingsWidget.dart
+++ lib/widgets/settingsWidget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:package_info/package_info.dart';
 
 // -------------- my packages ------------------
 import '../services/authService.dart';
@@ -12,12 +13,18 @@ class SettingsWidget extends StatefulWidget {
 }
 
 class _SettingsWidgetState extends State<SettingsWidget> {
+  String _version;
   @override
   Widget build(BuildContext context) {
     AuthService _authService = new AuthService();
     StateVariablesModel svm = Provider.of<StateVariablesModel>(context);
-   // Size size = MediaQuery.of(context).size;
-
+    // Size size = MediaQuery.of(context).size;
+// ------------------ package info ------------------------
+    PackageInfo.fromPlatform().then((PackageInfo packageInfo) {
+      setState(() {
+        _version = packageInfo.version;
+      });
+    });
     return SingleChildScrollView(
       child: Column(
         children: [
@@ -30,13 +37,13 @@ class _SettingsWidgetState extends State<SettingsWidget> {
             child: Column(
               children: [
                 Padding(
-                  padding:
-                      const EdgeInsets.only(left: 2.0, right: 2.0, top: 8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      InkWell(
-                          child: Text(
+                  padding: const EdgeInsets.only(
+                      left: 2.0, right: 2.0, top: 8.0, bottom: 8.0),
+                  child: InkWell(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
                             'Log Out',
                             style: TextStyle(
                               fontFamily: 'Nunito',
@@ -44,28 +51,30 @@ class _SettingsWidgetState extends State<SettingsWidget> {
                               //color: Colors.red,
                             ),
                           ),
-                          onTap: () {
-                            String msg = 'Do you really want to log out?';
-                            showLogOutDialog(
-                              context,
-                              msg,
-                              _authService,
-                              svm, /*userIdStatus*/
-                            );
-                          }),
-                    ],
-                  ),
+                          Icon(Icons.arrow_forward_ios),
+                        ],
+                      ),
+                      onTap: () {
+                        String msg = 'Do you really want to log out?';
+                        showLogOutDialog(
+                          context,
+                          msg,
+                          _authService,
+                          svm, /*userIdStatus*/
+                        );
+                      }),
                 ),
                 Divider(
                   thickness: 2.0,
                 ),
                 Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      InkWell(
-                          child: Text(
+                  padding: const EdgeInsets.only(
+                      left: 2.0, right: 2.0, top: 8.0, bottom: 8.0),
+                  child: InkWell(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
                             'Change Course Set Up',
                             style: TextStyle(
                               fontFamily: 'Nunito',
@@ -73,20 +82,21 @@ class _SettingsWidgetState extends State<SettingsWidget> {
                               //color: Colors.red,
                             ),
                           ),
-                          onTap: () {
-                            Navigator.of(context)
-                                .pushNamed('/CourseSetUpWidget');
-                          }),
-                    ],
-                  ),
+                          Icon(Icons.arrow_forward_ios),
+                        ],
+                      ),
+                      onTap: () {
+                        Navigator.of(context).pushNamed('/CourseSetUpWidget');
+                      }),
                 ),
                 Divider(
                   thickness: 2.0,
                 ),
                 Padding(
-                  padding: const EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.only(
+                      left: 2.0, right: 2.0, top: 8.0, bottom: 8.0),
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text(
                         'Dark Mode',
@@ -96,6 +106,10 @@ class _SettingsWidgetState extends State<SettingsWidget> {
                           //color: Colors.red,
                         ),
                       ),
+                      Switch(
+                        value: true,
+                        onChanged: (_) {},
+                      ),
                     ],
                   ),
                 ),
@@ -103,25 +117,172 @@ class _SettingsWidgetState extends State<SettingsWidget> {
                   thickness: 2.0,
                 ),
                 Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      InkWell(
-                          child: Text(
-                            'FAQ\'s',
+                  padding: const EdgeInsets.only(
+                      left: 2.0, right: 2.0, top: 8.0, bottom: 8.0),
+                  child: InkWell(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'FAQ\'s about this app',
+                          style: TextStyle(
+                            fontFamily: 'Nunito',
+                            fontSize: 16,
+                            //color: Colors.red,
+                          ),
+                        ),
+                        Icon(Icons.arrow_forward_ios),
+                      ],
+                    ),
+                  ),
+                ),
+                Divider(
+                  thickness: 2.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 2.0, right: 2.0, top: 8.0, bottom: 8.0),
+                  child: InkWell(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Update',
                             style: TextStyle(
                               fontFamily: 'Nunito',
                               fontSize: 16,
                               //color: Colors.red,
                             ),
                           ),
-                          onTap: () {}),
-                    ],
-                  ),
+                          Icon(Icons.arrow_forward_ios),
+                        ],
+                      ),
+                      onTap: () {}),
                 ),
                 Divider(
                   thickness: 2.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 2.0, right: 2.0, top: 8.0, bottom: 8.0),
+                  child: InkWell(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'About',
+                            style: TextStyle(
+                              fontFamily: 'Nunito',
+                              fontSize: 16,
+                              //color: Colors.red,
+                            ),
+                          ),
+                          Icon(Icons.arrow_forward_ios),
+                        ],
+                      ),
+                      onTap: () {}),
+                ),
+                Divider(
+                  thickness: 2.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 2.0, right: 2.0, top: 8.0, bottom: 8.0),
+                  child: InkWell(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Developer Details',
+                            style: TextStyle(
+                              fontFamily: 'Nunito',
+                              fontSize: 16,
+                              //color: Colors.red,
+                            ),
+                          ),
+                          Icon(Icons.arrow_forward_ios),
+                        ],
+                      ),
+                      onTap: () {}),
+                ),
+                Divider(
+                  thickness: 2.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 2.0, right: 2.0, top: 8.0, bottom: 8.0),
+                  child: InkWell(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Delete Account',
+                            style: TextStyle(
+                              fontFamily: 'Nunito',
+                              fontSize: 16,
+                              //color: Colors.red,
+                            ),
+                          ),
+                          Icon(Icons.arrow_forward_ios),
+                        ],
+                      ),
+                      onTap: () {}),
+                ),
+                Divider(
+                  thickness: 2.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 2.0,
+                    right: 2.0,
+                    top: 8.0,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Chandu - ',
+                        style: TextStyle(
+                          fontFamily: 'Nunito',
+                          fontSize: 16,
+                          //color: Colors.red,
+                        ),
+                      ),
+                      Text(
+                        '$_version',
+                        style: TextStyle(
+                          fontFamily: 'Nunito',
+                          fontSize: 16,
+                          //color: Colors.red,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Made with ',
+                      style: TextStyle(
+                        fontFamily: 'Nunito',
+                        // fontSize: 16,
+                        //color: Colors.red,
+                      ),
+                    ),
+                    Icon(
+                      Icons.favorite,
+                      color: Colors.pink,
+                    ),
+                    Text(
+                      ' by Abhjit Apotikar.',
+                      style: TextStyle(
+                        fontFamily: 'Nunito',
+                        //  fontSize: 16,
+                        //color: Colors.red,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
--- lib/widgets/termsAndConditionsWidget.dart
+++ lib/widgets/termsAndConditionsWidget.dart
@@ -17,7 +17,7 @@ class TermsAndConditionsWidget extends StatelessWidget {
           'Terms & Conditions',
           style: TextStyle(
             fontFamily: 'Nunito',
-            fontSize: 24,
+            fontSize: 20,
             fontWeight: FontWeight.bold,
           ),
         ),
@@ -26,86 +26,110 @@ class TermsAndConditionsWidget extends StatelessWidget {
           color: Colors.white,
         ),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              '1) By agreeing to this T&C\'s, You agree to let us identify you with your email.',
-              style: TextStyle(fontFamily: 'Nunito'),
-            ),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              '2) By agreeing to this T&C\'s, You agree to recieve any account related correspondence emails.',
-              style: TextStyle(fontFamily: 'Nunito'),
-            ),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              '3) By agreeing to this T&C\'s, You declare that you by your own will is sharing your email with us.',
-              style: TextStyle(fontFamily: 'Nunito'),
-            ),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              '4) By agreeing to this T&C\'s, You agree that you will not decompile, reverse-engineer or in any way mess up with this application\'s source code.',
-              style: TextStyle(fontFamily: 'Nunito'),
-            ),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              '5) By agreeing to this T&C\'s, You take the whole responsibility for installing & running this application on your device and the consequences that may arise therein.',
-              style: TextStyle(fontFamily: 'Nunito'),
-            ),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              '6) By agreeing to this T&C\'s, You declare that you are fully aware of the fact that whatever the information is presented with this application\'s aide is correct to the best of the developer\'s knowledge and will not be liable for scrutiny and the developer will in no way be held responsible for any error or incorrectness or ill consequences that may arise henceforth. ',
-              style: TextStyle(fontFamily: 'Nunito'),
-            ),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  '* ',
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  '1) By agreeing to this T&C\'s, You agree to let us identify you with your email.',
                   style: TextStyle(fontFamily: 'Nunito'),
                 ),
-                Text('T&C',
-                    style: TextStyle(
-                        fontFamily: 'Nunito', fontWeight: FontWeight.bold)),
-                Text(' stands as Terms&Conditions',
-                    style: TextStyle(fontFamily: 'Nunito')),
-              ],
-            ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  '2) By agreeing to this T&C\'s, You agree to recieve any account related correspondence emails.',
+                  style: TextStyle(fontFamily: 'Nunito'),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  '3) By agreeing to this T&C\'s, You declare that you by your own will is sharing your email with us.',
+                  style: TextStyle(fontFamily: 'Nunito'),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  '4) By agreeing to this T&C\'s, You agree that you will not decompile, reverse-engineer or in any way mess up with this application\'s source code.',
+                  style: TextStyle(fontFamily: 'Nunito'),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  '5) By agreeing to this T&C\'s, You take the whole responsibility for installing & running this application on your device and the consequences that may arise therein.',
+                  style: TextStyle(fontFamily: 'Nunito'),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  '6) By agreeing to this T&C\'s, You declare that you are fully aware of the fact that whatever the information is presented to you with this application\'s aide is correct to the best of the developer\'s knowledge and will not be liable for scrutiny and the developer will in no way be held responsible for any error or incorrectness or ill consequences that may arise henceforth. ',
+                  style: TextStyle(fontFamily: 'Nunito'),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  '7) By agreeing to this T&C\'s, You declare that you know and you agree to the condition that the developer can delete my account with this app at any moment of time without any prior notice. ',
+                  style: TextStyle(fontFamily: 'Nunito'),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  '8) By agreeing to this T&C\'s, You declare that you know and you agree that the use of this application for any commercial activity is strictly prohibited and you will only use this application for personal use only. ',
+                  style: TextStyle(fontFamily: 'Nunito'),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      '* ',
+                      style: TextStyle(fontFamily: 'Nunito'),
+                    ),
+                    Text('T&C',
+                        style: TextStyle(
+                            fontFamily: 'Nunito', fontWeight: FontWeight.bold)),
+                    Text(' stands as Terms&Conditions',
+                        style: TextStyle(fontFamily: 'Nunito')),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
